dd7907b51cd9f66680b4e0b95293fff2297446ec
Jake Whitling <jake.whitling99@gmail.com>
2024-03-13 18:54:54 +0000

    updating the input.py file

--- input.py
+++ input.py
@@ -32,6 +32,26 @@ class MyGridLayout(GridLayout):
         self.colour = TextInput(multiline = False)
         self.add_widget(self.colour)
 
+        # Create a submit button
+        self.submit = Button(text = 'Submit', font_size = 32)
+        #Bind the button
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        name = self.name.text
+        pizza = self.pizza.text
+        colour = self.colour.text
+
+        #print(f"Hello {name}, you like {pizza} pizza, and your favourite colour is {colour}!")
+        #print to the screen
+        self.add_widget(Label(text = f"Hello {name}, you like {pizza} pizza, and your favourite colour is {colour}!"))
+
+        #clear the boxes
+        self.name.text = ''
+        self.pizza.text = ''
+        self.colour.text = ''
+
 class MyApp(App):
     def build(self):
         return MyGridLayout()
